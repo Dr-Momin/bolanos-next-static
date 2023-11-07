a031751e99fde54b0c5c6f437c5e299e4e525ccb
Momin Khan <mmomin.bscsf19@iba-suk.edu.pk>
2023-11-08 00:25:40 +0500

    ADD: section three

--- app/page.js
+++ app/page.js
@@ -1,12 +1,17 @@
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
+import Footer from "@/components/Footer";
+import HeroTwo from "@/components/HeroTwo";
 
 export default function Home() {
   return (
     <main className="">
       <Navbar />
       <Hero />
+
+      <HeroTwo />
+      <Footer />
     </main>
   );
 }
---
+++ components/HeroTwo.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const HeroTwo = () => {
+  return (
+    <div>
+      <div className="bg-primary px-6 py-12 text-center  md:px-12 lg:text-left">
+        <div className="w-100 mx-auto sm:max-w-2xl md:max-w-3xl lg:max-w-5xl xl:max-w-6xl">
+          <div className="grid items-center gap-12 lg:grid-cols-2">
+            <div className="w-80 mt-12 lg:mt-0">
+              <h1 className="mt-2 mb-16 font-semibold tracking-tight md:text-3xl text-center bg-[#ffffff1f] py-6 px-8 rounded-xl text-white">
+                How Bolanos <br />  Trucks Works ?
+              </h1>
+              <p className="text-center">Watch out the video</p>
+            </div>
+            <div className="mb-12 lg:mb-0">
+              <img src="/images/img.png"
+                   className="w-full rounded-lg shadow-lg dark:shadow-black/20" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroTwo;
